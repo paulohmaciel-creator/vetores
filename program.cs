9d3bd26e0e2b4c31dc370a3232892f9001f0cccd
Paulo <paulo_h_maciel@estudante.sesisenai.org.br>
2026-06-22 21:14:15 -0300

    q1

---
+++ program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+class Program
+{
+    static void Main()
+    {
+        double[] notas = new double[5];
+        double soma = 0;
+        double media;
+
+        // Entrada de dados
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Informe a nota: ");
+            notas[i] = double.Parse(Console.ReadLine());
+        }
+
+        // Processamento (soma)
+        for (int i = 0; i < 5; i++)
+        {
+            soma += notas[i];
+        }
+
+        // Cálculo da média
+        media = soma / 5;
+
+        // Saída
+        Console.WriteLine("Soma das notas: " + soma);
+        Console.WriteLine("Média final: " + media);
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
