28348edec2f64c62b22e415a18f24f127acd58fe
Paulo <paulo_h_maciel@estudante.sesisenai.org.br>
2026-06-22 21:25:29 -0300

    q4

--- program.cs
+++ program.cs
@@ -4,30 +4,28 @@ class Program
 {
     static void Main()
     {
-        double[] gastos = new double[7];
-        double total = 0, maior, menor;
+        double[] horas = new double[7];
+        double total = 0;
+        int meta = 0, naoMeta = 0;
 
         for (int i = 0; i < 7; i++)
         {
-            Console.Write("Informe o gasto do dia: ");
-            gastos[i] = double.Parse(Console.ReadLine());
+            Console.Write("Informe as horas estudadas no dia: ");
+            horas[i] = double.Parse(Console.ReadLine());
         }
-
-        maior = menor = gastos[0];
 
         for (int i = 0; i < 7; i++)
         {
-            total += gastos[i];
+            total += horas[i];
 
-            if (gastos[i] > maior) maior = gastos[i];
-            if (gastos[i] < menor) menor = gastos[i];
+            if (horas[i] >= 2)
+                meta++;
+            else
+                naoMeta++;
         }
 
-        double media = total / 7;
-
-        Console.WriteLine("Total gasto na semana: R$ " + total);
-        Console.WriteLine("Média diária: R$ " + media);
-        Console.WriteLine("Maior gasto: R$ " + maior);
-        Console.WriteLine("Menor gasto: R$ " + menor);
+        Console.WriteLine("Total de horas: " + total);
+        Console.WriteLine("Dias com meta atingida: " + meta);
+        Console.WriteLine("Dias sem meta: " + naoMeta);
     }
 }
