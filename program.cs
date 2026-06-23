8b36feba7169959c6a26243d5879fff44a53d887
Paulo <paulo_h_maciel@estudante.sesisenai.org.br>
2026-06-22 21:51:03 -0300

    q9

--- program.cs
+++ program.cs
@@ -4,24 +4,35 @@ class Program
 {
     static void Main()
     {
-        int[] votos = new int[12];
-        int pizza = 0, hamburguer = 0, pastel = 0;
+        string[] tarefas = new string[5];
+        int[] prioridade = new int[5];
+        int baixa = 0, media = 0, alta = 0;
 
-        for (int i = 0; i < 12; i++)
+        for (int i = 0; i < 5; i++)
         {
-            Console.Write("Informe o voto: ");
-            votos[i] = int.Parse(Console.ReadLine());
+            Console.Write("Tarefa: ");
+            tarefas[i] = Console.ReadLine();
+
+            Console.Write("Prioridade: ");
+            prioridade[i] = int.Parse(Console.ReadLine());
         }
 
-        for (int i = 0; i < 12; i++)
+        for (int i = 0; i < 5; i++)
         {
-            if (votos[i] == 1) pizza++;
-            else if (votos[i] == 2) hamburguer++;
-            else if (votos[i] == 3) pastel++;
+            if (prioridade[i] == 1) baixa++;
+            else if (prioridade[i] == 2) media++;
+            else if (prioridade[i] == 3) alta++;
         }
 
-        Console.WriteLine("Pizza: " + pizza);
-        Console.WriteLine("Hambúrguer: " + hamburguer);
-        Console.WriteLine("Pastel: " + pastel);
+        Console.WriteLine("Baixa: " + baixa);
+        Console.WriteLine("Média: " + media);
+        Console.WriteLine("Alta: " + alta);
+
+        Console.WriteLine("Tarefas de alta prioridade:");
+        for (int i = 0; i < 5; i++)
+        {
+            if (prioridade[i] == 3)
+                Console.WriteLine("- " + tarefas[i]);
+        }
     }
 }
