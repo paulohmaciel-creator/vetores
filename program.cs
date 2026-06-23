f556c77c9aebac9eab24212db8162731857e599f
Paulo <paulo_h_maciel@estudante.sesisenai.org.br>
2026-06-22 21:52:20 -0300

    q10

--- program.cs
+++ program.cs
@@ -4,35 +4,32 @@ class Program
 {
     static void Main()
     {
-        string[] tarefas = new string[5];
-        int[] prioridade = new int[5];
-        int baixa = 0, media = 0, alta = 0;
+        string[] nomes = new string[8];
+        string busca;
+        bool encontrado = false;
 
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 8; i++)
         {
-            Console.Write("Tarefa: ");
-            tarefas[i] = Console.ReadLine();
-
-            Console.Write("Prioridade: ");
-            prioridade[i] = int.Parse(Console.ReadLine());
+            Console.Write("Informe o nome: ");
+            nomes[i] = Console.ReadLine();
         }
 
-        for (int i = 0; i < 5; i++)
+        Console.Write("Informe o nome para busca: ");
+        busca = Console.ReadLine();
+
+        for (int i = 0; i < 8; i++)
         {
-            if (prioridade[i] == 1) baixa++;
-            else if (prioridade[i] == 2) media++;
-            else if (prioridade[i] == 3) alta++;
+            if (nomes[i] == busca)
+            {
+                Console.WriteLine("Encontrado na posição " + (i + 1));
+                encontrado = true;
+                break;
+            }
         }
 
-        Console.WriteLine("Baixa: " + baixa);
-        Console.WriteLine("Média: " + media);
-        Console.WriteLine("Alta: " + alta);
-
-        Console.WriteLine("Tarefas de alta prioridade:");
-        for (int i = 0; i < 5; i++)
+        if (!encontrado)
         {
-            if (prioridade[i] == 3)
-                Console.WriteLine("- " + tarefas[i]);
+            Console.WriteLine("Estudante não encontrado.");
         }
     }
 }
